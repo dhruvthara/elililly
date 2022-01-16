0482207dedf0ea5937eda645eaf27f0aca58fae4
dhruvthara <narmada.v@smnetserv.com>
2022-01-16 08:15:11 +0530

    materiAlize added

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,20 +5,46 @@ import React from 'react';import {
   Link
 } from "react-router-dom";
 
+import { Navbar, NavItem, Icon, Col } from 'react-materialize';
+
 const Header = props => {
-  return <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/customerdata">Customer Data</Link>
-          </li>
-        </ul>
-      </nav>
+  return <Navbar
+  alignLinks="right"
+  brand={<a className="brand-logo" href="#">Logo</a>}
+  id="mobile-nav"
+  menuIcon={<Icon>menu</Icon>}
+  options={{
+    draggable: true,
+    edge: 'left',
+    inDuration: 250,
+    onCloseEnd: null,
+    onCloseStart: null,
+    onOpenEnd: null,
+    onOpenStart: null,
+    outDuration: 200,
+    preventScrolling: true
+  }}
+>
+  <NavItem href="">
+    Getting started
+  </NavItem>
+  <NavItem href="components.html">
+    Components
+  </NavItem>
+</Navbar>
+  // return <nav>
+  //       <ul>
+  //         <li>
+  //           <Link to="/">Home</Link>
+  //         </li>
+  //         <li>
+  //           <Link to="/login">Login</Link>
+  //         </li>
+  //         <li>
+  //           <Link to="/customerdata">Customer Data</Link>
+  //         </li>
+  //       </ul>
+  //     </nav>
 };
 
 export default Header;
